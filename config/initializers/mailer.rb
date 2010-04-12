ecbf40a88fd686d2a91c5a001c5ff16805c9f123
Tobias Crawley <tcrawley@gmail.com>
2010-04-11 21:40:14 -0400

    Don't config email - use the heroku sendgrid settings.

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,2 +1,2 @@
-mailer_options = YAML.load_file("#{RAILS_ROOT}/config/mailer.yml")
-ActionMailer::Base.smtp_settings = mailer_options
+#mailer_options = YAML.load_file("#{RAILS_ROOT}/config/mailer.yml")
+#ActionMailer::Base.smtp_settings = mailer_options
